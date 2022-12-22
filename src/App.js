94e9471c0fef23a7667898b50e0f3c1ae0fcaff0
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-22 12:55:06 +0530

    Created file structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./index.css";
+import Home from './routes/Home';
+import About from './routes/About';
+import Project from './routes/Project';
+import Contact from './routes/Contact';
 
 function App() {
   return (
     <div >
-      
+      This is app.js
+      <Home/>
     </div>
   );
 }
