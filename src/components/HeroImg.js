45a73a556fd28cf41c5567ef747be1cf8c5547aa
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-24 19:37:26 +0530

    Added initial page and css.

---
+++ src/components/HeroImg.js
@@ -0,0 +1,30 @@
+import './HeroImgStyles.css';
+import React from 'react'
+import introImage from '../assets/img.jpeg';
+import { Link } from 'react-router-dom';
+
+const HeroImg = () => {
+  return (
+    <div className='hero'>
+        <div className="mask">
+            <img className='intro-img'
+            src={introImage}
+            alr={introImage}/>
+        </div>
+        <div className="content">
+            <p>HI. I'M A FREELANCER</p>
+            <h1>React Developer.</h1>
+            <div>
+                <Link to="/project" className='btn'>
+                    Projects
+                </Link>
+                <Link to="/contact" className='btn btn-light'>
+                    Contact
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg;
