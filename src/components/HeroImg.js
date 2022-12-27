1f7daf01401b520db67a9d48e813fb8546a2c9a0
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-27 18:20:17 +0530

    Added Typical.

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,6 +1,7 @@
 import "./HeroImgStyles.css";
 import IntroImg from "../assets/Ibg.jpg";
 import React from 'react'
+import Typical from 'react-typical';
 import {Link} from "react-router-dom";
 
 const HeroImg = () => {
@@ -11,14 +12,49 @@ const HeroImg = () => {
 
     </div>
     <div className="content">
-        <p>HI, I am Shloka Mahesheka</p>
-        <h1>Full Stack Developer</h1>
+    <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm{" "}
+              <span className="highlighted-text">Shloka Mahesheka</span>
+            </span>
+          </div>
+    <div className="details">
+            <span className="text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical 
+                className="typical"
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Coder 🔴 ",
+                    1000,
+                    "Full Stack Developer 💻",
+                    1000,
+                    "Java Dev 😎",
+                    1000,
+                    "React Developer ✅",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations.
+              </span>
+            </span>
+          </div>
+
         <div>
-            <Link to ="/project"
-            className="btn">Projects</Link>
+            {/* <Link to ="/project"
+            className="btn">Projects</Link> */}
+            <a href="ShlokaResume.pdf" download="Shloka Mahesheka Resume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
                <Link to ="/contact"
             className="btn btn-light">
-                Contact</Link>
+                Hire Me!</Link>
         </div>
     </div>
     </div>
