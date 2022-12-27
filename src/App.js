5b672fd8714483e83ab6c17610f22c4a91bbc5bb
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-27 16:01:47 +0530

    Deploy 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Routes, Route} from 'react-router-dom';
 function App() {
   return (
    <>
-   <Routes>
+   <Routes >
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
     <Route path='/project' element={<Project/>}/>
